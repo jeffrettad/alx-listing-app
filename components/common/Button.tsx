50d5cce739d14ab68eededba8dd6291474c08e70
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-10-30 10:37:56 -0500

    add  Button component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,12 +1,1 @@
 
-
-import React from 'react';
-import { ButtonProps } from '../../interfaces';
-
-const Button: React.FC<ButtonProps> = ({ label, onClick, className }) => (
-  <button onClick={onClick} class ={px-4 py-2 bg-blue-600 text-white rounded ${className}}>
-    {label}
-  </button>
-);
-
-export default Button;
