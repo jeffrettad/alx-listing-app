be9c1496f96688625c477e95c3e8d1aa351ca4ea
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-10-30 10:40:50 -0500

    add interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,11 +1,1 @@
-export interface ButtonProps {
-  label: string;
-  onClick: () => void;
-  className?: string;
-}
 
-export interface CardProps {
-  title: string;
-  description: string;
-  imageUrl: string;
-}
